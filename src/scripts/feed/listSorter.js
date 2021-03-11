677b8cae11d5e694d2256c1d800fb9cdb2494959
Coachbre <brekonan1@gmail.com>
2021-03-11 17:05:38 -0500

    still working on event listeners- list sorting

--- src/scripts/feed/listSorter.js
+++ src/scripts/feed/listSorter.js
@@ -1,17 +1,21 @@
 export const listSorter = () => {
 
-    // HTML to be returned to Entry list component
+
+    // HTML to be returned to daily journal component
     return `
-        <div class="selector">
-            <div class="selector__item">
-                Posts since <select id="monthSelection">
-                    <option>10</option>
-                    <option>07</option>
-                    <option>03</option>
-                    <option>01</option>
-                </select>
-                <span id="postCount">0</span>
-            </div>
-        </div>
+         <div class="sort">
+                <div class="sort__item">
+                    Posts since 
+                    <select id="monthSelection">
+                         <option>2020</option>
+                         <option>2019</option>
+                         <option>2018</option>
+                         <option>2017</option>
+                    </select>
+                   <span id="postCount">0</span>
+                </div>
+            </div>
     `
 }
+console.log("HELLO");
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import { getEntries } from "./data/DataManager.js"
 import { EntryListComponent } from "./feed/journalEntryList.js"
+import { listSorter } from "./feed/listSorter.js"
+
+const applicationElement = document.querySelector(".dailyJournal");
 
 
 // shows journal entry logs in list form
@@ -14,12 +17,16 @@ const showEntryList = () => {
 
 // event listener- list sort (by month)
 applicationElement.addEventListener("change", event => {
-    if (event.target.id === "yearSelection") {
-      const yearAsNumber = parseInt(event.target.value)
+    if (event.target.id === "monthSelection") {
+      const monthAsNumber = parseInt(event.target.value)
   
-      console.log(`User wants to see posts since ${yearAsNumber}`)
+
+      listSorter(monthAsNumber);
     }
   })
+
+
+  
 
 
 
@@ -30,6 +37,7 @@ applicationElement.addEventListener("change", event => {
 // Runnn
 const startJournal = () => {
     showEntryList();
+ 
 
 }
 
